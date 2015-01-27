a82691d21391c5884c24647e85a79b1594a0b14f
cgallen <cgallen@opennms.org>
2015-01-27 15:42:47 +0000

    first commit of EDD OSGi licence manager

---
+++ licence-manager/easy-digital-downloads-osgi-licences/eddosgilicences.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Plugin Name: easy-digital-downloads-osgi-licences
+ * Version: 1.0
+ * Plugin URI: http://opennms.co.uk/
+ * Description: Adds OSGi licence management to Easy Digital Downloads.
+ * Author: Craig Gallen
+ * Author URI: http://www.craiggallen.com/
+ * Requires at least: 4.0
+ * Tested up to: 4.0
+ *
+ * Text Domain: eddosgilicences
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Craig Gallen
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load plugin class files
+require_once( 'includes/class-eddosgilicences.php' );
+require_once( 'includes/class-eddosgilicences-settings.php' );
+
+// Load plugin libraries
+require_once( 'includes/lib/class-eddosgilicences-admin-api.php' );
+require_once( 'includes/lib/class-eddosgilicences-post-type.php' );
+require_once( 'includes/lib/class-eddosgilicences-taxonomy.php' );
+
+/**
+ * Returns the main instance of eddOsgiLicences to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object eddOsgiLicences
+ */
+function eddOsgiLicences () {
+	$instance = eddOsgiLicences::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = eddOsgiLicences_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+eddOsgiLicences();
---
+++ licence-manager/easy-digital-downloads-osgi-licences/readme.txt
@@ -0,0 +1,43 @@
+=== eddOsgiLicences ===
+Contributors: gallen
+Donate link: http://www.caiggallen.com/donate
+Tags: wordpress, plugin, template
+Requires at least: 3.9
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easy digital Downloads plugin to enable selling of OSGi Licencces
+
+== Description ==
+
+This plugin extends the Easy digital Downloads plugin to allow it to inteact with the OSGi Licence manager
+running in a Karaf instance
+
+== Installation ==
+
+Installing "eddOsgiLicences" can be done either by searching for "eddOsgiLicences" via the "Plugins > Add New" screen in your WordPress dashboard, or by using the following steps:
+
+1. Download the plugin via WordPress.org
+1. Upload the ZIP file through the 'Plugins > Add New > Upload' screen in your WordPress dashboard
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= What is the plugin for? =
+
+The OSGi licence manager controls which OSGi modules start up in a KARAF container.
+This plugin allows programmers to create and sell licences for use with the manager
+
+== Changelog ==
+
+= 1.0 =
+* 2015-01-27
+* Initial release
+
+== Upgrade Notice ==
+
+= 1.0 =
+* 2015-01-27
+* Initial release
