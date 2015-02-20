14e8f11148c3fcc9cd030890192ed9a7ad66f403
unknown <cgallen@opennms.org>
2015-02-20 13:07:58 +0000

    added apache and GPL licences

---
+++ licence-manager/README.txt
@@ -0,0 +1,7 @@
+==Licence==
+
+All of the Java / OSGi modules in this project are licensed under the Apache 2 Licence
+(This is to make the library widely usable with multiple OSGi projects and shopping carts)
+
+All the Wordpress Plugins created in this project  are licensed under GPL V2+ 
+(in accordance with Wordpress)
--- licence-manager/org.opennms.licencemanager/src/main/java/org/opennms/karaf/licencemgr/metadata/jaxb/ErrorMessage.java
+++ licence-manager/org.opennms.licencemanager/src/main/java/org/opennms/karaf/licencemgr/metadata/jaxb/ErrorMessage.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 OpenNMS Group Inc., Entimoss ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.opennms.karaf.licencemgr.metadata.jaxb;
 
 import java.io.PrintWriter;
@@ -24,13 +39,13 @@ public class ErrorMessage {
 	/**
 	 * Helper constructor to build an error reply 
 	 * (Error handling suggestion taken from http://www.codingpedia.org/ama/error-handling-in-rest-api-with-jersey/)
-	 * @param status holds redundantly the HTTP error status code, so that the developer can “see” 
-	 *        it without having to analyze the response’s header
+	 * @param status holds redundantly the HTTP error status code, so that the developer can â€œseeâ€� 
+	 *        it without having to analyze the responseâ€™s header
 	 * @param code this is an internal code specific to the API (should be more relevant for business exceptions)
-	 * @param message short description of the error, what might have cause it and possibly a “fixing” proposal
+	 * @param message short description of the error, what might have cause it and possibly a â€œfixingâ€� proposal
 	 * @param link points to an online resource, where more details can be found about the error
 	 * @param developerMessage detailed message, containing additional data that might be relevant to the developer. 
-	 *       This should only be available when the “debug” mode is 
+	 *       This should only be available when the â€œdebugâ€� mode is 
 	 *       switched on and could potentially contain stack trace information or something similar
 	 * @return EventGatewayErrorMessage jaxb object to include in the xml reply
 	 */
@@ -46,10 +61,10 @@ public class ErrorMessage {
 	/**
 	 * Helper constructor to build an error reply 
 	 * (Error handling suggestion taken from http://www.codingpedia.org/ama/error-handling-in-rest-api-with-jersey/)
-	 * @param status holds redundantly the HTTP error status code, so that the developer can “see” 
-	 *        it without having to analyze the response’s header
+	 * @param status holds redundantly the HTTP error status code, so that the developer can â€œseeâ€� 
+	 *        it without having to analyze the responseâ€™s header
 	 * @param code this is an internal code specific to the API (should be more relevant for business exceptions)
-	 * @param message short description of the error, what might have cause it and possibly a “fixing” proposal
+	 * @param message short description of the error, what might have cause it and possibly a â€œfixingâ€� proposal
 	 * @param link points to an online resource, where more details can be found about the error
 	 * @param exception is converted to stacktrace and appended as developer message
 	 * @return EventGatewayErrorMessage jaxb object to include in the xml reply
