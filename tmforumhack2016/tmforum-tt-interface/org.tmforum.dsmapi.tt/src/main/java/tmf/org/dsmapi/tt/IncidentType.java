090051dd6766326f30261cdd94f9d5c25d996ba5
cgallen <cgallen@opennms.org>
2016-02-01 22:48:58 +0000

    original tmforum tt client

---
+++ tmforumhack2016/tmforum-tt-interface/org.tmforum.dsmapi.tt/src/main/java/tmf/org/dsmapi/tt/IncidentType.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tmf.org.dsmapi.tt;
+
+/**
+ * none that i know
+ * @author pierregauthier ATMFORUM
+ */
+public enum IncidentType {
+    Submitted, Acknowledged, InProgress, Resolved, Closed, Rejected;  //; is optional
+}
