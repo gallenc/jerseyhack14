538a47f3b45a9395f391470aafdd77ac778044ba
cgallen <cgallen@opennms.org>
2016-02-02 16:07:14 +0000

    first working client code

--- tmforumhack2016/tmforum-tt-interface/org.tmforum.dsmapi.tt/src/main/java/tmf/org/dsmapi/tt/client/TroubleTicketClientV2.java
+++ tmforumhack2016/tmforum-tt-interface/org.tmforum.dsmapi.tt/src/main/java/tmf/org/dsmapi/tt/client/TroubleTicketClientV2.java
@@ -1,36 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package tmf.org.dsmapi.tt.client;
 
 import java.io.IOException;
-
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.JsonGenerator;
-import org.codehaus.jackson.jaxrs.JacksonJaxbJsonProvider;
-import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
 import org.codehaus.jackson.map.AnnotationIntrospector;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import tmf.org.dsmapi.tt.TroubleTicket;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientRequest.Builder;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.json.JSONConfiguration;
-//import com.sun.jersey.api.client.WebResource.Builder;
 
 /**
  * Jersey REST client generated for REST resource:TroubleTicketFacadeREST
@@ -86,60 +69,36 @@ public class TroubleTicketClientV2 {
 	public TroubleTicket createTroubleTicket(TroubleTicket tt){
 		if(tt==null) throw new RuntimeException("trouble ticket cannot be null");
 		if(baseUri==null) throw new RuntimeException("baseUri cannot be null");
-		LOG.debug("calling createTroubleTicket tt="+tt.toString()+ " baseUri="+baseUri);
+		
+		LOG.debug("calling createTroubleTicket baseUri="+baseUri+"\n"
+				+ "trouble ticket="+tt.toString()+ "");
 
 		ClientConfig clientConfig = new DefaultClientConfig();
-		//clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-			//clientConfig.getClasses().add(JacksonJsonProvider.class);
-		//clientConfig.getClasses().add(JacksonJaxbJsonProvider.class);
 		
-		ObjectMapper mapper = new ObjectMapper();
-	    AnnotationIntrospector introspector = new JaxbAnnotationIntrospector();
-	    // make deserializer use JAXB annotations (only)
-	    mapper.getDeserializationConfig().setAnnotationIntrospector(introspector);
-
-	    String jsonString =null;
-	    try {
-			jsonString = mapper.writeValueAsString( tt );
-			
-		} catch (JsonGenerationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		// only create json string if debugging
+		if (LOG.isDebugEnabled()){
+			ObjectMapper mapper = new ObjectMapper();
+		    AnnotationIntrospector introspector = new JaxbAnnotationIntrospector();
+		    // make deserializer use JAXB annotations (only)
+		    mapper.getDeserializationConfig().withAnnotationIntrospector(introspector);
+		    String jsonString =null;
+				try {
+					jsonString = mapper.writeValueAsString( tt );
+				} catch (IOException e) {
+					LOG.debug("problem logging jsonString",e);
+				}
+			LOG.debug("calling jsonString="+jsonString);
 		}
-		
-		
-		
-		
-		
+
 		Client client = Client.create(clientConfig);
 		
 		WebResource webResourcePost = client.resource(baseUri).path("troubleTicket");
-		WebResource.Builder builder = webResourcePost.type(MediaType.APPLICATION_JSON_TYPE).accept(MediaType.APPLICATION_JSON_TYPE);
-		
-		//WebResource.Builder builder = webResourcePost.accept(MediaType.APPLICATION_JSON_TYPE);
-		
-		//webResourcePost.accept(MediaType.APPLICATION_JSON_TYPE);
-		//webResourcePost.type(MediaType.APPLICATION_JSON_TYPE);
-		
-		//WebResource res = c.resource("http://localhost:5984/");
-		
-		//System.out.println(builder.get(String.class));
-		
-		
-	
 		LOG.debug("calling createTroubleTicket uri="+webResourcePost.getURI().toString());
-		LOG.debug("calling json call="+jsonString);
-
-		ClientResponse response = builder.post(ClientResponse.class, jsonString);
+		
+		WebResource.Builder builder = webResourcePost.type(MediaType.APPLICATION_JSON_TYPE).accept(MediaType.APPLICATION_JSON_TYPE);
+		ClientResponse response = builder.post(ClientResponse.class, tt);
 
 		if (response.getStatus() != 201) {
-			
 			LOG.debug("createTroubleTicket response status= "+response.getStatus()+" status info=" +response.getStatusInfo().toString());
 			return null;
 		}
@@ -151,32 +110,36 @@ public class TroubleTicketClientV2 {
 	}
 
 
-	//    troubleTicketUpdate
-	//    troubleTicketPatch
+	/**
+	 * troubleTicketUpdate
+	 * @param tt
+	 * @return
+	 */
+	public TroubleTicket updateTroubleTicket(TroubleTicket tt){
+		throw new UnsupportedOperationException("this operation is not yet implemented");
+		//return null;
+	}
+	
+	/**
+	 * troubleTicketPatch
+	 * @param tt
+	 * @return
+	 */
+	public TroubleTicket patchTroubleTicket(TroubleTicket tt){
+		throw new UnsupportedOperationException("this operation is not yet implemented");
+		//return null;
+	}
+	
+/**
+ *  troubleTicketFind
+ * @param tt
+ * @return
+ */
+	public TroubleTicket findTroubleTicket(TroubleTicket tt){
+		throw new UnsupportedOperationException("this operation is not yet implemented");
+		//return null;
+	}
 
-	//    troubleTicketCreate
-	//    troubleTicketFind
-
-//	public void temp(){
-//
-//		ClientConfig clientConfig = new DefaultClientConfig();
-//		//clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-//		Client client = Client.create(clientConfig);
-//
-//		// GET request to findBook resource with a query parameter
-//		String getBookURL = "http://env-0693795.jelastic.servint.net/DSTroubleTicket/api/troubleTicketManagement/v2/troubleTicket/2";
-//		// WebResource webResourceGet = client.resource(getBookURL).queryParam("id", "1");
-//		WebResource webResourceGet = client.resource(getBookURL);
-//		//WebResource webResourceGet = client.resource(getBookURL);
-//		ClientResponse response = webResourceGet.get(ClientResponse.class);
-//		TroubleTicket responseEntity = response.getEntity(TroubleTicket.class);
-//
-//		if (response.getStatus() != 200) {
-//			throw new WebApplicationException();
-//		}
-//
-//		System.out.println(responseEntity.toString());
-//	}
 
 
 
--- tmforumhack2016/tmforum-tt-interface/org.tmforum.dsmapi.tt/src/test/java/tmf/org/dsmapi/tt/client/test/TroubleTicketClientTest.java
+++ tmforumhack2016/tmforum-tt-interface/org.tmforum.dsmapi.tt/src/test/java/tmf/org/dsmapi/tt/client/test/TroubleTicketClientTest.java
@@ -24,27 +24,6 @@ public class TroubleTicketClientTest {
 
 	private String baseUri="http://env-0693795.jelastic.servint.net/DSTroubleTicket/api/troubleTicketManagement/v2";
 
-	// http://env-0693795.jelastic.servint.net/DSTroubleTicket/api/troubleTicketManagement/v2/troubleTicket/2
-
-	//	@Test
-	//	public void test() {
-	//		TroubleTicketClientV2 client = new TroubleTicketClientV2(ttclientbase);
-	//		
-	//		String id="2";
-	//		//TroubleTicket tt= client.find(TroubleTicket.class, id); 
-	//	}
-	//	
-	//	@Test
-	//	public void test2() {
-	//		TroubleTicketClientV2 client = new TroubleTicketClientV2(ttclientbase);
-	//
-	//		//String result= client.countREST(); 
-	//		
-	//		//System.out.println("ticket count="+result);
-	//	}
-	// http://env-0693795.jelastic.servint.net/DSTroubleTicket/api/troubleTicketManagement/v2/2
-	// http://env-0693795.jelastic.servint.net/DSTroubleTicket/api/troubleTicketManagement/v2/troubleTicket/2
-
 	@Test
 	public void getKnownTroubleTicket(){
 		LOG.debug("test getKnownTroubleTicket start");
@@ -74,8 +53,6 @@ public class TroubleTicketClientTest {
 		}
 		LOG.debug("test getUnKnownTroubleTicket end");
 	}
-	
-	
 	
 	@Test
 	public void createTroubleTicketTest(){
@@ -117,18 +94,6 @@ public class TroubleTicketClientTest {
 		}
 		LOG.debug("test createTroubleTicketTest end");
 	}
-
-//	@Test
-//	public void test3(){
-//		try{
-//			TroubleTicketClientV2 client = new TroubleTicketClientV2();
-//			client.temp();
-//		} catch (Exception e){
-//			LOG.error("test getUnKnownTroubleTicket error:",e);
-//			throw new RuntimeException(e);
-//		}
-//		LOG.debug("test getUnKnownTroubleTicket end");
-//	}
 
 
 }
