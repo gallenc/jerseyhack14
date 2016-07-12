ff4e31e0c9b609f33fb2054aab193b5c5acfd716
cgallen <cgallen@opennms.org>
2016-07-12 18:52:54 +0100

    working basic project with swagger ui for diagnostics

---
+++ tmforumhack2016/tmforum-sla-api/main-module/src/main/java/org/opennms/plugins/tmforum/sla/Temp.java
@@ -0,0 +1,12 @@
+package org.opennms.plugins.tmforum.sla;
+
+public class Temp {
+	public Temp(){
+		super();
+		System.out.println("Hello there!!! 7");
+	}
+	
+	public void destroyMethod(){
+		System.out.println("goodbye!!!");
+	}
+}
